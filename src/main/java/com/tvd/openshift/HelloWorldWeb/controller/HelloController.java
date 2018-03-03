abedce36c39b521771fd1f493c4557d742df23e4
Nick Jagger <Nicholas.Jagger@trivadis.com>
2018-03-03 12:29:52 +0100

    Changed message output

--- src/main/java/com/tvd/openshift/HelloWorldWeb/controller/HelloController.java
+++ src/main/java/com/tvd/openshift/HelloWorldWeb/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 	
     @RequestMapping("/")
     public String index() {
-        return "Greetings from TechEvent!";
+        return "Greetings from TechEvent 2!";
     }
 }
